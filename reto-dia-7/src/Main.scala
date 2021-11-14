86022c3c264b73dc4a7bb5decaafe2c2b6adc60c
chejito <smndz81@gmail.com>
2021-11-14 21:54:31 +0100

    If de Main sustituidos por pattern matching

--- reto-dia-7/src/Main.scala
+++ reto-dia-7/src/Main.scala
@@ -12,6 +12,9 @@ object Main {
     val estudiante3 = Estudiante("estudiante3", "", "estudiante3@ejemplo.com",
       Set("JavaScript", "HTML/CSS", "React"), Set("proyectoFrontEndOfertas"))
 
+    val estudiante4 = Estudiante("", null, "estudiante4@ejemplo.com",
+      Set("JavaScript", "Java"), Set("proyectoRetosDiarios"))
+
     val invitado1 = Invitado("invitado1", "passwordInvitado1", "invitado1@ejemplo.com", 1800000)
 
     val invitado2 = Invitado("invitado2", "passwordInvitado2", "invitado2@ejemplo.com", 3600000)
@@ -20,7 +23,8 @@ object Main {
 
     val administrador2 = Administrador("administrador2", "passwordAdministrador2", "administrador2@ejemplo.com", 2)
 
-    val usuarios = Set(estudiante1, estudiante2, estudiante3, invitado1, invitado2, administrador1, administrador2)
+    val usuarios = Set(estudiante1, estudiante2, estudiante3, estudiante4,
+      invitado1, invitado2, administrador1, administrador2)
 
     usuarios.foreach {
       case Invitado(nombre, _, _, _) =>
@@ -29,9 +33,14 @@ object Main {
 
       case Estudiante(nombre, password, _, _, _) =>
         println(s"\nNombre: $nombre - Password: $password")
-        if (estaVacio(nombre) || estaVacio(password))
-          println("No tienes un nombre y/o password")
-        else println(s"Bienvenid@ a OpenVitae, $nombre")
+        val errorCredenciales: String = "No tienes un nombre de usuario y/o password"
+        estaVacio(nombre) match {
+          case true => println(errorCredenciales)
+          case false => estaVacio(password) match {
+            case true => println(errorCredenciales)
+            case false => println(s"Bienvenid@ a OpenVitae, $nombre")
+          }
+        }
 
       case Administrador(nombre, _, _, nivel) =>
         println(s"\nNombre: $nombre - Nivel: $nivel")
