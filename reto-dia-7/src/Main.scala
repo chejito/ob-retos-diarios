c99f667d9c4a811d48fed61f58c0f3291b9b2e9f
chejito <smndz81@gmail.com>
2021-11-14 22:13:28 +0100

    Refactorización de Main

--- reto-dia-7/src/Main.scala
+++ reto-dia-7/src/Main.scala
@@ -5,26 +5,19 @@ object Main {
 
     val estudiante1 = Estudiante("estudiante1", "passwordEstudiante1", "estudiante1@ejemplo.com",
       Set("Java", "Spring"), Set("proyectoBackendOfertas", "proyectoGithubApi", "proyectoRetosDiarios"))
-
     val estudiante2 = Estudiante(null, "passwordEstudiante2", "estudiante2@ejemplo.com",
       Set("JavaScript", "HTML/CSS", "React"), Set("proyectoFrontEndOfertas", "proyectoRetosDiarios"))
-
     val estudiante3 = Estudiante("estudiante3", "", "estudiante3@ejemplo.com",
       Set("JavaScript", "HTML/CSS", "React"), Set("proyectoFrontEndOfertas"))
-
     val estudiante4 = Estudiante("", null, "estudiante4@ejemplo.com",
       Set("JavaScript", "Java"), Set("proyectoRetosDiarios"))
 
-    val invitado1 = Invitado("invitado1", "passwordInvitado1", "invitado1@ejemplo.com", 1800000)
-
-    val invitado2 = Invitado("invitado2", "passwordInvitado2", "invitado2@ejemplo.com", 3600000)
+    val invitado = Invitado("invitado1", "passwordInvitado1", "invitado1@ejemplo.com", 1800000)
 
     val administrador1 = Administrador("administrador1", "passwordAdministrador1", "administrador1@ejemplo.com", 1)
-
     val administrador2 = Administrador("administrador2", "passwordAdministrador2", "administrador2@ejemplo.com", 2)
 
-    val usuarios = Set(estudiante1, estudiante2, estudiante3, estudiante4,
-      invitado1, invitado2, administrador1, administrador2)
+    val usuarios = Set(estudiante1, estudiante2, estudiante3, estudiante4, invitado, administrador1, administrador2)
 
     usuarios.foreach {
       case Invitado(nombre, _, _, _) =>
@@ -34,11 +27,13 @@ object Main {
       case Estudiante(nombre, password, _, _, _) =>
         println(s"\nNombre: $nombre - Password: $password")
         val errorCredenciales: String = "No tienes un nombre de usuario y/o password"
-        estaVacio(nombre) match {
-          case true => println(errorCredenciales)
-          case false => estaVacio(password) match {
-            case true => println(errorCredenciales)
-            case false => println(s"Bienvenid@ a OpenVitae, $nombre")
+        nombre match {
+          case null => println(errorCredenciales)
+          case "" => println(errorCredenciales)
+          case _ => password match {
+            case null => println(errorCredenciales)
+            case "" => println(errorCredenciales)
+            case _ => println(s"Bienvenid@ a OpenVitae, $nombre")
           }
         }
 
@@ -50,12 +45,4 @@ object Main {
 
   }
 
-  def estaVacio(texto: String): Boolean = {
-    texto match {
-      case null => true
-      case "" => true
-      case _ => false
-    }
-
-  }
 }
